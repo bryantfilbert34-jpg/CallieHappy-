6386b70e69f365c817f1417d85ec9819a122dbb1
bryantfilbert34-jpg <bryantfilbert34@gmail.com>
2026-07-31 02:54:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+const btn = document.getElementById("loveBtn");
+const hearts = document.getElementById("hearts");
 
+function createHeart() {
+    const heart = document.createElement("div");
+    heart.className = "heart";
+    heart.innerHTML = "💖";
+
+    heart.style.left = Math.random() * window.innerWidth + "px";
+    heart.style.animationDuration = (3 + Math.random() * 4) + "s";
+
+    hearts.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 7000);
+}
+
+setInterval(createHeart, 250);
+
+btn.onclick = () => {
+    alert(`💖 Happy Girlfriend Day 💖
+
+Untuk Callie 🌸
+
+Terima kasih sudah hadir dalam hidupku.
+
+Aku bersyukur bisa mengenalmu.
+Semoga kita selalu bersama,
+tertawa bersama,
+dan saling mencintai setiap hari.
+
+I Love You Forever ❤️`);
+};
